19a496cfa9ced46f0433c62c6e1db14b63c58dba
Siwa12100 <jean.marcillac12@gmail.com>
2023-07-26 00:09:06 +0200

    ajout de modifications

--- src/main/java/dev/siwa/tinnu/Tinnu.java
+++ src/main/java/dev/siwa/tinnu/Tinnu.java
@@ -4,6 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.World;
+
+import java.time.LocalTime;
 import java.util.Objects;
 
 public final class Tinnu extends JavaPlugin {
@@ -13,8 +16,53 @@ public final class Tinnu extends JavaPlugin {
     @Override
     public void onEnable() {
         tinnu = this;
+
+        Horloge h =new Horloge(tinnu);
+
+        for (World w : Bukkit.getWorlds()){
+            System.out.println("\n\n - " + w.getName());
+        }
+
         System.out.println("\n\n ---> Lancement du plugin Tinnu.\n\n");
-        initialisationCommandes();
+
+        for (World w : Bukkit.getWorlds()){
+            if (w.getName().equals("survie")){
+                h.lancement(w);
+                System.out.println(" ---> Lancement de la synchro jour / nuit dans le monde survie");
+            }
+
+            if (w.getName().equals("donjons")){
+                h.lancement(w);
+                System.out.println("---> Lancement de la synchro jour / nuit dans le monde donjons");
+            }
+
+            if (w.getName().equals("itayas1")){
+                h.lancement(w);
+                System.out.println("---> Lancement de la synchro jour / nuit dans le monde Itayas1");
+            }
+
+            if (w.getName().equals("eventides1")){
+                h.lancement(w);
+                System.out.println("---> Lancement de la synchro jour / nuit dans le monde eventides1");
+            }
+        }
+
+        // Pour l'Agora
+        /*for (World w : Bukkit.getWorlds()){
+            if (w.getName().equals("world")){
+                h.lancement(w);
+                System.out.println(" ---> Lancement de la synchro jour / nuit dans le monde de l'Agora");
+            }
+
+            if (w.getName().equals("antonio_world")){
+                h.lancement(w);
+                System.out.println("---> Lancement de la synchro jour / nuit dans le monde de Antonio");
+            }
+        }*/
+
+        initialisationCommandes(h);
+        System.out.println("---> Commandes de tinnu initialisées.");
+        System.out.println("---> Fin du OnEnable de Tinnu");
     }
 
     @Override
@@ -22,9 +70,14 @@ public final class Tinnu extends JavaPlugin {
         System.out.println("\n\n ---> Extinction du plugin Tinnu.\n\n");
     }
 
-    private void initialisationCommandes() {
-        Objects.requireNonNull(getCommand("tinnu")).setExecutor(new Horloge(this));
+    private void initialisationCommandes(Horloge h) {
+        Objects.requireNonNull(getCommand("tinnu")).setExecutor(h);
         //getCommand("tn").setExecutor(new Horloge());
+    }
+
+    public LocalTime getHeureActuelle() {
+        return LocalTime.now();
+        //return heureActuelleFactice;
     }
 
     public Tinnu() {
